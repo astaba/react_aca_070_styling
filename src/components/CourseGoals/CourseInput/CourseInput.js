7bfe42757df759c3f1913ffb55da94ffbdcc9d20
Astar <bahouidi99@gmail.com>
2023-07-04 16:40:23 +0100

    Complete section 7

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,26 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+// import { styled } from "styled-components";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+// import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+//   & label {
+//     color: ${(props) => (!props.invalid ? "inherit" : "red")};
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (!props.invalid ? "#ccc" : "red")};
+//     background-color: ${(props) => (!props.invalid ? "tranparent" : "rgb(255, 185, 127)")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [goalValue, setGoalValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const handleGoalChange = (event) => {
+    const targetValue = event.target.value;
+    if (targetValue.trim().length > 0) setIsValid(true);
+    setGoalValue(targetValue);
   };
 
-  const formSubmitHandler = event => {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (goalValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(goalValue);
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+    <form onSubmit={handleFormSubmit}>
+      {/* <FormControl className={!isValid && "invalid"}> */}
+      {/* <FormControl invalid={!isValid}> */}
+      <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" onChange={handleGoalChange} />
       </div>
+      {/* </FormControl> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,69 @@
+/**
+ * styling with css-modules
+ * In the prop: className={styles.button}
+ * we are still writing the plain string of a css selector,
+ * however the css-module system will spice it up so as to be
+ * unique to that component like "Button_button__hashCode".
+ */
 import React from 'react';
-
-import './Button.css';
+import styles from './Button.module.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button}>
       {props.children}
     </button>
   );
 };
 
 export default Button;
+
+/**
+ * styling with styled-components
+ */
+// import { styled } from "styled-components";
+
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width: 680px) {
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+// export default Button;
+
+/**
+ * styling with plain css
+ */
+// import React from 'react';
+
+// import './Button.css';
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button">
+//       {props.children}
+//     </button>
+//   );
+// };
+
+// export default Button;
