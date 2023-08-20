e6f4b53746293396df9fb973ef2c96b6e35fcdbd
Astar <bahouidi99@gmail.com>
2023-08-20 10:47:21 +0100

    Refactor styled components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,36 +1,43 @@
 import React, { useState } from "react";
-// import { styled } from "styled-components";
+import { styled } from "styled-components";
 
 import Button from "../../UI/Button/Button";
 // import "./CourseInput.css";
 import styles from "./CourseInput.module.css";
 
-// const FormControl = styled.div`
-//   margin: 0.5rem 0;
+const StyledDiv = styled.div`
+  margin: 0.5rem 0;
 
-//   & label {
-//     color: ${(props) => (!props.invalid ? "inherit" : "red")};
-//     font-weight: bold;
-//     display: block;
-//     margin-bottom: 0.5rem;
-//   }
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
 
-//   & input {
-//     display: block;
-//     width: 100%;
-//     border: 1px solid ${(props) => (!props.invalid ? "#ccc" : "red")};
-//     background-color: ${(props) => (!props.invalid ? "tranparent" : "rgb(255, 185, 127)")};
-//     font: inherit;
-//     line-height: 1.5rem;
-//     padding: 0 0.25rem;
-//   }
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
 
-//   & input:focus {
-//     outline: none;
-//     background: #fad0ec;
-//     border-color: #8b005d;
-//   }
-// `;
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    background-color: rgb(255, 185, 127);
+    border-color: red;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [goalValue, setGoalValue] = useState("");
@@ -53,13 +60,13 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={handleFormSubmit}>
-      {/* <FormControl className={!isValid && "invalid"}> */}
-      {/* <FormControl invalid={!isValid}> */}
-      <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
+      <StyledDiv className={!isValid && "invalid"}>
+        {/* <StyledDiv invalid={!isValid}> */}
+        {/* <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}> */}
         <label>Course Goal</label>
         <input type="text" onChange={handleGoalChange} />
-      </div>
-      {/* </FormControl> */}
+        {/* </div> */}
+      </StyledDiv>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,51 +5,61 @@
  * however the css-module system will spice it up so as to be
  * unique to that component like "Button_button__hashCode".
  */
-import React from 'react';
-import styles from './Button.module.css';
+// import React from 'react';
+// import styles from './Button.module.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className={styles.button}>
-      {props.children}
-    </button>
-  );
-};
+// const Button = props => {
+//   return (
+//     <button type={props.type} className={styles.button}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
-export default Button;
+// export default Button;
 
 /**
  * styling with styled-components
  */
-// import { styled } from "styled-components";
+import React from 'react';
+import { styled } from "styled-components";
 
-// const Button = styled.button`
-//   width: 100%;
-//   font: inherit;
-//   padding: 0.5rem 1.5rem;
-//   border: 1px solid #8b005d;
-//   color: white;
-//   background: #8b005d;
-//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-//   cursor: pointer;
+const StyledButton = styled.button`
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-//   @media (min-width: 680px) {
-//     width: auto;
-//   }
+  &:focus {
+    outline: none;
+  }
 
-//   &:focus {
-//     outline: none;
-//   }
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+  
+  @media (min-width: 680px) {
+    width: auto;
+  }
 
-//   &:hover,
-//   &:active {
-//     background: #ac0e77;
-//     border-color: #ac0e77;
-//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-//   }
-// `;
+`;
 
-// export default Button;
+const Button = props => {
+  return (
+    <StyledButton type={props.type}>
+      {props.children}
+    </StyledButton>
+  );
+};
+
+export default Button;
 
 /**
  * styling with plain css
